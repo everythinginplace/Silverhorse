e91463db83a5506a624ef9de048ef0786dc3d28e
Dave Clark <dubsola@gmail.com>
2020-09-11 06:14:00 +0800

    remove old code

--- src/shared/data.item.service.ts
+++ src/shared/data.item.service.ts
@@ -13,30 +13,5 @@ export class DataItemService {
             Users: this.jsonPlaceholderService.getUsers(),
             Posts: this.jsonPlaceholderService.getPosts()
         })
-        
-
-        // tis works, just want to not test         
-        // var posts = this.jsonPlaceholderService.getPosts().subscribe((posts) => {
-        //     console.log(" posts = " + posts);
-        //     var albums = this.jsonPlaceholderService.getAlbums().subscribe((data) => {
-        //         console.log("albums = " + data);
-        //         var posts = this.jsonPlaceholderService.getUsers().subscribe((data) => {
-        //             console.log("users = " + data);
-
-        //         });
-        //     });
-        // });        // return [
-        //     {
-        //       "post": "first post",
-        //       "album": "quidem molestiae enim",
-        //       "userName": "username"
-        //     },
-            
-        //     {
-        //       "post": "second post",
-        //       "album": "veni vidi vici", 
-        //       "userName": "username2"
-        //     }
-        //   ];
     }
   }
